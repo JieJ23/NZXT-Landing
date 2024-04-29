04371acc4c12b0ba9a106c8330675af7f03932f8
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2024-04-29 18:33:45 -0400

    Fixed path

--- src/Components/Heroes2.jsx
+++ src/Components/Heroes2.jsx
@@ -3,7 +3,7 @@ import { Typography, Card } from "@material-tailwind/react";
 export default function Heroes2() {
   return (
     <section className="w-full bg-transparent h-[70%] flex flex-col relative my-10">
-      <div className="absolute h-full w-full top-0 left-0 bg-black xl:bg-[url('heroes/hero2-bg.png')] xl:bg-cover xl:bg-no-repeat xl: -z-10" />
+      <div className="absolute h-full w-full top-0 left-0 bg-black xl:bg-[url('/heroes/hero2-bg.png')] xl:bg-cover xl:bg-no-repeat xl: -z-10" />
 
       <div className="basis-1/2 z-20 px-4 py-8 xl:py-56 xl:px-32 xl:w-1/2">
         <img src="/heroes/hero2-header.png" alt="Heroes 2" />
