16bfbf9de6cf1318f114f7f855709b6a85565fad
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2024-04-30 10:42:46 -0400

    Reconstructed to grid layout

--- src/Components/Selection.jsx
+++ src/Components/Selection.jsx
@@ -1,4 +1,5 @@
 import { Typography, Card, Button, Rating } from "@material-tailwind/react";
+import { Wrapper } from "./RestCards";
 
 const allPCOptions = [
   {
@@ -63,10 +64,7 @@ const allPCOptions = [
 
 function CardPC({ product, name, cpu, gpu, price, img, type, rating }) {
   return (
-    <Card
-      className="border-0 px-4 h-full flex flex-col justify-between"
-      shadow={false}
-    >
+    <Card className="col-span-6 md:col-span-4" shadow={false}>
       <img
         src={`/PC/${img}`}
         alt="Player 1 PC"
@@ -76,49 +74,50 @@ function CardPC({ product, name, cpu, gpu, price, img, type, rating }) {
             : "bg-gray-400"
         }
       />
-      <Typography className="font-bold" variant="h6">
-        {name}
-      </Typography>
-      <Typography className="font-normal" variant="small">
-        {product}
-      </Typography>
-      <div className="flex items-center">
-        <Rating value={rating % 2 === 0 ? 4 : 5} readonly />
-        <Typography variant="small" className="">{`(${rating})`}</Typography>
-      </div>
-      <div className="border-y-2 py-6">
-        <Typography variant="small" className="font-bold">
-          Key Specs
+      <div className="w-full h-full flex flex-col justify-between mt-4">
+        <Typography className="font-bold" variant="h6" color="black">
+          {name}
         </Typography>
-        <Typography variant="small">Windows 11 Home</Typography>
-        <Typography variant="small">{cpu}</Typography>
-        <Typography variant="small">{gpu}</Typography>
+        <Typography className="font-normal" variant="small">
+          {product}
+        </Typography>
+        <div className="flex items-center">
+          <Rating value={rating % 2 === 0 ? 4 : 5} readonly />
+          <Typography variant="small" className="">{`(${rating})`}</Typography>
+        </div>
+        <div className="border-y-2 py-6">
+          <Typography variant="small" className="font-bold">
+            Key Specs
+          </Typography>
+          <Typography variant="small">Windows 11 Home</Typography>
+          <Typography variant="small">{cpu}</Typography>
+          <Typography variant="small">{gpu}</Typography>
+        </div>
+        <Typography className="font-normal py-4" variant="small">
+          {`$${price}.00`}
+        </Typography>
+        <Button className="bg-indigo-600">SHOP</Button>
       </div>
-      <Typography className="font-normal py-4" variant="small">
-        {`$${price}.00`}
-      </Typography>
-      <Button className="bg-indigo-600">SHOP</Button>
     </Card>
   );
 }
 
 export default function PCSelections() {
   return (
-    <div className="my-24 px-14 max-w-[1200px] mx-auto">
-      <section className="my-8">
-        <Typography variant="h1" className="text-center">
-          NZXT Prebuilt Gaming PCs
-        </Typography>
-        <Typography
-          variant="h6"
-          color="blue-gray"
-          className="text-center opacity-60 px-6 lg:px-12 w-full lg:w-1/2 mx-auto"
-        >
-          We use the latest generation performance components and configurations
-          to get you gaming, fast.
-        </Typography>
-      </section>
-      <section className="grid grid-cols-2 lg:grid-cols-3 gap-y-10 place-items-center">
+    <>
+      <Wrapper>
+        <div className="col-span-12 mt-10">
+          <Typography className="text-center text-5xl font-bold">
+            NZXT Prebuilt Gaming PCs
+          </Typography>
+          <Typography className="w-full lg:w-1/2 mx-auto text-center text-lg font-normal opacity-60">
+            We use the latest generation performance components and
+            configurations to get you gaming, fast.
+          </Typography>
+        </div>
+      </Wrapper>
+
+      <Wrapper>
         {allPCOptions.map((obj, index) => (
           <CardPC
             name={obj.name}
@@ -132,7 +131,7 @@ export default function PCSelections() {
             rating={obj.rating}
           />
         ))}
-      </section>
-    </div>
+      </Wrapper>
+    </>
   );
 }
